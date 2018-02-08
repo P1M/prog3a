f75c08f4ef2c9b858ffada962cdc09c584825139
P1M <renato.barbosa.pim.pereira@hotmail.com>
2018-02-07 22:30:35 -0300

    adicionei a conexao ao banco

--- dba.php
+++ dba.php
@@ -1,16 +1,15 @@
- <?php
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "123pimpim";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=prog3", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
-    }
+}
 catch(PDOException $e)
-    {
+{
     echo "Connection failed: " . $e->getMessage();
-    }
-?>
+}
