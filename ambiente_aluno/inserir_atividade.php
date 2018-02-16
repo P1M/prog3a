7c06e65850b8e1ca563ee364fdeaf166ae70a659
abilenevieira <abilene20@gmail.com>
2018-02-16 08:40:57 -0300

    abilene insere no banco de dados as atividades

---
+++ ambiente_aluno/inserir_atividade.php
@@ -0,0 +1,24 @@
+
+<?php
+
+include 'Database.php';
+
+
+$titulo = $_POST['titulo'];
+$cargaHora = $_POST['cargaHora'];
+$ano = $_POST['ano'];
+$tipo = $_POST ['tipo'];
+
+$sql = "INSERT INTO cadastro VALUES ";
+$sql .= "('$titulo', '$cargaHora', '$ano', '$tipo')";
+
+
+if($conexao->query($sql)  === true){
+    echo "Cliente cadastrado com sucesso!";
+
+}else{
+    echo "erro: " . $sql . "<br>" . $conexao->error;
+}
+$conexao->close() ;
+
+?>
