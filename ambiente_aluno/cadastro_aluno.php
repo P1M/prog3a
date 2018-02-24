57f4aa86852c82d6a53125ac0c39ead2493d1459
abilenevieira <abilene20@gmail.com>
2018-02-24 08:44:55 -0300

    abilene cadastro aluno php

--- ambiente_aluno/cadastro_aluno.php
+++ ambiente_aluno/cadastro_aluno.php
@@ -8,25 +8,39 @@
 
 include 'Database.php';
 
+/* Attempt MySQL server connection. Assuming you are running MySQL
 
-$ra = $_POST['ra'];
-$curso = $_POST['curso'];
-$cpf = $_POST['cpf'];
-$nome = $_POST['nome'];
-$email = $_POST ['email'];
-$senha = $_POST ['senha'];
+server with default setting (user 'root' with no password) */
+//$link = mysqli_connect("localhost", "root", "123pimpim", "prog3");
 
-$sql = "INSERT INTO cadastro VALUES ";
-$sql .= "('$ra', '$nome', '$cpf',  '$email', '$curso', '$senha')";
+include('Database.php');
+$db = Database::getInstance();
+$link = $db->getConnection();
 
-
-if($conexao->query($sql)  === true){
-    echo "Aluno cadastrado com sucesso!";
-
-}else{
-    echo "erro: " . $sql . "<br>" . $conexao->error;
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-$conexao->close() ;
+// Escape user inputs for security
+$ra = mysqli_real_escape_string($link, $_REQUEST['ra']);
+$nome = mysqli_real_escape_string($link, $_REQUEST['nome']);
+$cpf = mysqli_real_escape_string($link, $_REQUEST['cpf']);
+$email = mysqli_real_escape_string($link, $_REQUEST ['email']);
+$curso = mysqli_real_escape_string($link, $_REQUEST['curso']);
+$senha = mysqli_real_escape_string($link, $_REQUEST ['senha']);
+$senha = md5($senha);
+
+$sql = "INSERT INTO aluno (RA,nome, cpf, email,curso,senha) VALUES ('$ra', '$nome', '$cpf',  '$email', '$curso', '$senha')";
+
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+// close connection
+mysqli_close($link);
+
 
 ?>
