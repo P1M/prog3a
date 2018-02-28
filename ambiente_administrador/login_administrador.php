57721356b5e93279c118f549d23610c822c60fc6
Brendon Locatelli <brendonlocatelli@hotmail.com>
2018-02-27 23:19:01 -0300

    Reajustes nas configs e layout do cad_coor e login_adm
    Ajudstada a checagem sql usuário ao acessar sql
    Layout padronizado pronto para ser inserido no site
    E outros

---
+++ ambiente_administrador/login_administrador.php
@@ -0,0 +1,38 @@
+<?php
+include('Database.php');
+$db = Database::getInstance();
+$link = $db->getConnection();
+// Check connection
+
+if($link === false){
+
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+
+}
+
+session_start();
+
+$senha = mysqli_real_escape_string($link, $_REQUEST['senha']);
+$email = mysqli_real_escape_string($link, $_REQUEST['email']);
+
+$sql = "SELECT CoordenadorAdministrador.email, Senha.senha
+FROM CoordenadorAdministrador
+LEFT JOIN Senha ON CoordenadorAdministrador.email = Senha.CoordenadorAdministrador_email
+WHERE Senha.senha LIKE md5('$senha')
+AND CoordenadorAdministrador.email LIKE '$email'
+AND CoordenadorAdministrador.tipo LIKE '1' ;";
+
+// $sql = "SELECT senha FROM Senha WHERE senha LIKE md5('$senha');";
+
+
+
+
+if(mysqli_num_rows(mysqli_query($link, $sql)) > 0)
+{
+    header("Location: cadastrar_coordenador.html"); }
+
+else{
+
+    echo "Usuário não encontrado. " . mysqli_error($link);
+
+}
