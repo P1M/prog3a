f3eb4bebbfb557e2cbf3446da9bd6cced1773950
juniormauricio23 <juniormauricio23@gmail.com>
2018-03-04 23:06:50 -0300

    concertado os login

--- ambiente_coordenador/login_coordenador.php
+++ ambiente_coordenador/login_coordenador.php
@@ -14,24 +14,21 @@ session_start();
 
 $senha = mysqli_real_escape_string($link, $_REQUEST['senha']);
 $email = mysqli_real_escape_string($link, $_REQUEST['email']);
+$_SESSION["email"] = $email;
 
 $sql = "SELECT CoordenadorAdministrador.email, Senha.senha
 FROM CoordenadorAdministrador
-LEFT JOIN Senha ON CoordenadorAdministrador.email = Senha.CoordenadorAdministrador_email
+LEFT JOIN Senha ON CoordenadorAdministrador.email LIKE Senha.CoordenadorAdministrador_email
 WHERE Senha.senha LIKE md5('$senha')
 AND CoordenadorAdministrador.email LIKE '$email'
-AND CoordenadorAdministrador.tipo LIKE '2' ;";
-
-// $sql = "SELECT senha FROM Senha WHERE senha LIKE md5('$senha');";
+AND CoordenadorAdministrador.tipo LIKE '1' ;";
 
 
-
-
-if(mysqli_num_rows(mysqli_query($link, $sql)) > 0)
+if(mysqli_num_rows(mysqli_query($link, $sql)) == 1)
 {
-    header("Location: cadastrar_professor.html"); }
+    header("Location: tela_coordenador.html");
 
-else{
+}else{
 
     echo "Usuário não encontrado. " . mysqli_error($link);
 
---
+++ ambiente_coordenador/tela_coordenador.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: mauricio
+ * Date: 04/03/18
+ * Time: 22:58
+ */
+header("Location: tela_coordenador.html");
