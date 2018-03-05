75322ce9f938e5238dda06aa1861293b8cedb700
abilenevieira <abilene20@gmail.com>
2018-03-05 11:34:33 -0300

    abilene gerar pdf

---
+++ ambiente_aluno/bib_dompdf/indexpdf.php
@@ -0,0 +1,43 @@
+﻿<?php	
+
+	$html = '<table border=1';	
+	$html .= '<thead>';
+	$html .= '<tr>';
+	$html .= '<th>ID</th>';
+	$html .= '<th>COD Transação</th>';
+	$html .= '<th>Tipo de Pagamento</th>';
+	$html .= '<th>Status da Transação</th>';
+	$html .= '<th>E-mail</th>';
+	$html .= '</tr>';
+	$html .= '</thead>';
+	$html .= '<tbody> AQUIVO GERADO EM PDF';
+	$html .= '</tbody>';
+	$html .= '</table';
+
+	
+	//referenciar o DomPDF com namespace
+	use Dompdf\Dompdf;
+
+	// include autoloader
+	require __DIR__ . '\autoload.inc.php';
+
+	//Criando a Instancia
+	$dompdf = new DOMPDF();
+	
+	// Carrega seu HTML
+	$dompdf->load_html('
+			<h1 style="text-align: center;">Relatório de Transações</h1>
+			'. $html .'
+		');
+
+	//Renderizar o html
+	$dompdf->render();
+
+	//Exibibir a página
+	$dompdf->stream(
+		"relatorio_celke.pdf", 
+		array(
+			"Attachment" => false //Para realizar o download somente alterar para true
+		)
+	);
+?>
---
+++ ambiente_aluno/fpdf/indexpdf2.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+require_once('fpdf.php');
+
+$pdf = new FPDF();
+
+$pdf->AddPage(); // padrão o relatório gerado é no formato A4
+
+$pdf->SetFont('Arial','B',16); // Arial, tamanho 16 em negrito, a fonte deve vir antes do texto
+
+$pdf->cell(0,10,'OI POVO',1,0,'C'); 
+
+ $pdf->ln(10);// pula 10 linhas, o valor 10 é por conta do valor da altura da célula
+
+$pdf->cell(40,10,'OI  GENTE'); 
+$pdf->Output(); //Saída para o navegador 
+
+?>
