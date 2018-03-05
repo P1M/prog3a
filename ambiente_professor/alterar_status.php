e50073c6849e7d9d980650be69a88776468c2a3d
Brendon Locatelli <brendonlocatelli@hotmail.com>
2018-03-05 11:23:17 -0300

    Começo da função que altera o status
    Falta receber as variaveis por POST e fazer as devidas conexões com o BD

---
+++ ambiente_professor/alterar_status.php
@@ -0,0 +1,13 @@
+<?php
+$RA = // receber por POST de recebe_link_e_avalia.phtml
+$titulo = // receber por POST recebe_link_e_avalia.phtml
+
+
+// Fazer conexão com o banco e executar o seguinte comando SQL:
+/* SQL
+UPDATE Atividades
+SET Status = $novoStatus
+WHERE Atividades.Aluno_RA LIKE $RA AND Atividades.Titulo LIKE $titulo;
+*/
+
+?>
