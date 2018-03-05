c10361fd41323140cd52f56a15112cd968046f3a
Brendon Locatelli <brendonlocatelli@hotmail.com>
2018-03-05 11:51:57 -0300

    pronto
    mas precisa testar

--- ambiente_professor/alterar_status.php
+++ ambiente_professor/alterar_status.php
@@ -1,13 +1,25 @@
 <?php
-$RA = // receber por POST de recebe_link_e_avalia.phtml
-$titulo = // receber por POST recebe_link_e_avalia.phtml
+include('../Database.php');
+$db = Database::getInstance();
+$link = $db->getConnection();
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 
 
-// Fazer conexão com o banco e executar o seguinte comando SQL:
-/* SQL
-UPDATE Atividades
+$RA = mysqli_real_escape_string($link, $_REQUEST['RA']);
+$titulo = mysqli_real_escape_string($link, $_REQUEST['titulo']);
+$novoStatus = mysqli_real_escape_string($link, $_REQUEST['novoStatus']);
+
+$sql = "UPDATE Atividades
 SET Status = $novoStatus
-WHERE Atividades.Aluno_RA LIKE $RA AND Atividades.Titulo LIKE $titulo;
-*/
+WHERE Atividades.Aluno_RA LIKE $RA AND Atividades.Titulo LIKE $titulo;";
+
 
 ?>
+
+ <form id="formulario" name="formulario" method="get" action="recebe_link_e_avalia.phtm">
+  Nome: <input id="nome" name="nome" type="hidden" value="<?php echo dechex($RA); $?>" />
+  <input id="btnenviar" name="btnenviar" type="submit" value="Voltar" />
+ </form>
